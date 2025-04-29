e26e3b2ca21edaf3bc28cdcf2ca6362c3609dec9
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-04-29 20:41:34 +0500

    Added TableEditor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import Navigation from "@/components/Navigation";
-import TableEditor from "@/components/TableEditor";
 
 export default function Home() {
   return (
