2d37a28d695414ed63a25e594be9c3cf6b537fa4
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-04-29 20:52:19 +0500

    ..

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,10 +11,13 @@ const Dashboard = async () => {
     <section className='flex flex-col w-full p-5'>
       <ProjectActionBar/>
       <div className='mt-4 flex md:flex-row flex-col gap-4 flex-wrap'>
-        {
+        { projects.length > 0 ? (
           projects.map((project)=>(
             <ProjectCard key={project.id} name={project.name} description={project.description ?? "No Description Provided"} created_at={project.created_at.toLocaleDateString()} slug={project.slug}/>
           ))
+        ) : (
+          <p className='text-white/65 mt-2 text-center'>You Don't have any projets yet.</p>
+        )
         }
       </div>
     </section>
